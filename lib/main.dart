25e8a310dcb4cfd5be49fff44d4c25f5f00b6ba6
Yasmin Lützelschwab <114688402+luetzyas@users.noreply.github.com>
2024-10-10 19:55:43 +0200

    add help to main.dart

---
+++ lib/main.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp( MyApp_Beginner());
+}
+
+class MyApp_Beginner extends StatelessWidget {
+  MyApp_Beginner({super.key});
+
+  //Variables https://dart.dev/language/variables
+  String name = "Esmeralda";
+  int age = 27;
+  double pi = 24/7;
+  bool isBeginner = true;
+
+  /*
+  Basic Math Operators:
+  1 + 1  = 2,   add
+  4 - 3  = 1,   subtractor
+  5 * 5  = 25,  multiply
+  10 / 2 = 5,   divide
+  5 ** 2 = 25,  to the power of x
+  5 % 2  = 1,   remainder (modulo)
+  */
+
+  /* https://dart.dev/language/operators
+  Comparison Operators:
+  1 == 1  = true,   equal to
+  1 != 1  = false,  not equal to
+  1 > 1   = false,  greater than
+  1 < 1   = false,  less than
+  1 >= 1  = true,   greater than or equal to
+  1 <= 1  = true,   less than or equal to
+  */
+
+  /*
+  Logical Operators:
+  1 && 1  = true,   and
+  1 || 1  = true,   or
+  ! 1     = false,  not
+  ?? null = true,   if-null
+  ^                 xor
+  */
+
+  @override
+  Widget build(BuildContext context) {
+    if (age >= 18){
+      print("you are an adult");
+    } else {
+      print("you are a minor");
+    }
+
+    return MaterialApp(
+      home: Scaffold(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
